b788c5603c1119d96628e4d43c31a77df9173ed1
Ivan Ferez <ferezivan@hotmail.com>
2023-11-08 21:59:53 -0300

    Metodos y servicios sin async awaiy - pasaje a promesas

--- Backend/src/routes/usuarios.routes.ts
+++ Backend/src/routes/usuarios.routes.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
+/* eslint-disable @typescript-eslint/promise-funciton-async */
 import { Router } from 'express'
-import { listarUsuarios, crearUsuarios, logearUsuarioPorEmailYPassword, obtenerUsuarioPorUsername, verificarUsuario, olvidarContrasenia } from '../controllers/usuarios.controller'
+import { listarUsuarios, crearUsuarios, logearUsuarioPorEmailYPassword, verificarUsuario, olvidarContrasenia } from '../controllers/usuarios.controller'
 const routes = Router()
 
 routes.get('/listarUsuarios', listarUsuarios)
 routes.post('/crearUsuario', crearUsuarios)
-routes.get('/obtenerUsuario', obtenerUsuarioPorUsername)
 routes.post('/logearUsuario', logearUsuarioPorEmailYPassword)
 routes.post('/verificarUsuario', verificarUsuario)
 routes.get('/olvidarContrasenia', olvidarContrasenia)
