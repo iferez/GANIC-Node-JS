176e91dcef5089691bcd45be5e43e454661cccaf
Cristian Feldman <cris@H3rzBSD.local>
2023-11-22 00:06:34 -0300

    mejora en el email

--- Backend/src/controllers/pago.controller.ts
+++ Backend/src/controllers/pago.controller.ts
@@ -11,11 +11,33 @@ const pagarOrdenDePago = (req: Request, res: Response): Response => {
       email: datosValidos.email,
       subject: 'Pago realizado correctamente',
       html: `<h1>Gracias por su compra</h1>
-      <p>El monto pagado es: $${datosValidos.monto}</p>
+      <p>El pago se ha realizado correctamente</p>
       <p>Los productos comprados son:</p>
-      <ul>
-          ${datosValidos.listaProductos.map((producto) => `<li>${producto.nombre}</li>`).join('')}
-      </ul>
+      <table style="width: 100%; border-collapse: collapse;">
+          <thead>
+              <tr>
+                  <th style="padding: 12px; text-align: left; border: 1px solid #ddd;">Nombre</th>
+                  <th style="padding: 12px; text-align: left; border: 1px solid #ddd;">Precio</th>
+                  <th style="padding: 12px; text-align: left; border: 1px solid #ddd;">Total</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr>
+                  ${datosValidos.listaProductos
+                .map(
+                  (producto) => `
+                  <tr>
+                      <td style="padding: 8px; border: 1px solid #ddd;">${producto.nombre}</td>
+                      <td style="padding: 8px; border: 1px solid #ddd;">$${producto.precio}</td>
+                      <td style="padding: 8px; border: 1px solid #ddd;"> </td>
+                  </tr>`
+                )
+                .join('')}
+                  <td colspan="2" style="padding: 8px; border: 1px solid #ddd; text-align: right;">Total:</td>
+                  <td style="padding: 8px; border: 1px solid #ddd;">$${datosValidos.monto}</td>
+              </tr>
+          </tbody>
+      </table>
       `
     })
     return res.status(200).json({ message: 'Pago realizado correctamente' })
